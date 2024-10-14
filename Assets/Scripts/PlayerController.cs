f25ff82642370a08421f397eca6357b309ffeae8
Drazen Drinic <drazendrinic@hotmail.com>
2024-10-14 23:08:59 +0200

    Clean: the player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
-    public float turnSpeed = 50.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 50.0f;
 
-    public float horizontalInput;
-    public float verticalInput;
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +22,9 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        // Move the vehicle forward
+        // Move the vehicle forward based on the vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Rotates the car based on the horizontal input
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
